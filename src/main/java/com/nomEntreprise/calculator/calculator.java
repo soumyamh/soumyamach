22952c78d89097046aa2b4c914a169517b3db46b
soumyamh <elmachbouhsoumya@gmail.com>
2025-12-06 12:07:31 +0100

    Rendre les méthodes add() et divide() statiques + mise à jour des tests

--- src/main/java/com/nomEntreprise/calculator/calculator.java
+++ src/main/java/com/nomEntreprise/calculator/calculator.java
@@ -4,11 +4,11 @@ package com.nomEntreprise.calculator;
 
 public class calculator {
 
-    public int add(int opG, int opD) {
+    public static int add(int opG, int opD) {
         return opG + opD;
     }
 
-    public int divide(int opG, int opD) {
+    public static int divide(int opG, int opD) {
         return opG / opD;
     }
 }
