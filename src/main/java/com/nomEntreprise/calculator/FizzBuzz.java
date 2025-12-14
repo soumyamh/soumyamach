fa08a3a1fbfbfa436b2ebacf440628c8c0e6042c
soumyamh <elmachbouhsoumya@gmail.com>
2025-12-14 18:30:49 +0100

    testNumber

--- src/main/java/com/nomEntreprise/calculator/FizzBuzz.java
+++ src/main/java/com/nomEntreprise/calculator/FizzBuzz.java
@@ -5,15 +5,7 @@ public class FizzBuzz {
 
 
         public static String de(int n) {
-            if (n % 15 == 0) {
-                return "FizzBuzz";
-            }
-            if (n % 3 == 0) {
-                return "Fizz";
-            }
-            if (n % 5 == 0) {
-                return "Buzz";
-            }
+
 
             return String.valueOf(n);
         }
--- src/test/java/com/nomEntreprise/calculator/FizzBuzzTest.java
+++ src/test/java/com/nomEntreprise/calculator/FizzBuzzTest.java
@@ -5,20 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
-    @Test
-    void testFizz() {
-        assertEquals("Fizz", FizzBuzz.de(3));
-    }
 
-    @Test
-    void testBuzz() {
-        assertEquals("Buzz", FizzBuzz.de(5));
-    }
-
-    @Test
-    void testFizzBuzz() {
-        assertEquals("FizzBuzz", FizzBuzz.de(15));
-    }
     @Test
         void testNumber() {
             assertEquals("2", FizzBuzz.de(2));
